397f238310e711700d92d4cf909c8298d77e18f6
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-11-19 04:37:12 +0800

    feat: Create Home Button component

---
+++ HomeButton.jsx
@@ -0,0 +1,8 @@
+export default function HomeButton() {
+    return (
+        <div className="home-button">
+            <img src="" alt="Home" className="home-button-icon"></img>
+            <div className="home-button-text">Memory Quest</div>
+        </div>
+    );
+}
