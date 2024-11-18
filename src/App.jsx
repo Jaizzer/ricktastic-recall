d149961e47af1558f77917dc932f5d62d566be23
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-11-18 20:56:35 +0800

    feat: Put the generation of random IDs inside useEffect to prevent ID generation every render

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import { useState } from 'react';
 export default function App() {
     const [characters, setCharacters] = useState(null);
 
-    // Obtain randomized IDs of characters to be fetched
-    const characterCount = 25;
-    const randomizedIDs = Array.from({ length: characterCount }, () =>
-        Math.floor(Math.random() * 800)
-    );
-
     useEffect(() => {
+        // Obtain randomized IDs of characters to be fetched
+        const characterCount = 25;
+        const randomizedIDs = Array.from({ length: characterCount }, () =>
+            Math.floor(Math.random() * 800)
+        );
+
         fetch('https://rickandmortyapi.com/api/character')
             .then((response) => response.json())
             .then((data) => setCharacters(data.results));
