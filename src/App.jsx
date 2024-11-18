37a515d0a7857d1404f6d4ffb409733c66707291
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-11-18 20:47:51 +0800

    feat: Implement feature to obtain 25 random characters

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import { useState } from 'react';
 export default function App() {
     const [characters, setCharacters] = useState(null);
 
+    // Obtain randomized IDs of characters to be fetched
+    const characterCount = 25
+    const randomizedIDs = Array.from({ length: characterCount }, () =>
+        Math.floor(Math.random() * 800)
+    );
+
+
     useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character')
             .then((response) => response.json())
