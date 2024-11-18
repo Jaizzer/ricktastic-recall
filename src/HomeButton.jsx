f2a46236e26c8b03e176cecbbee5d07294942867
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-11-19 05:12:39 +0800

    feat: Make HomeButton accept click event functionality

--- src/HomeButton.jsx
+++ src/HomeButton.jsx
@@ -1,6 +1,6 @@
-export default function HomeButton() {
+export default function HomeButton({ handleClick }) {
     return (
-        <div className="home-button">
+        <div className="home-button" onClick={handleClick}>
             <img src="" alt="Home" className="home-button-icon"></img>
             <div className="home-button-text">Memory Quest</div>
         </div>
