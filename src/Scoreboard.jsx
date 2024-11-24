67cf1c08bd427c5dc64d24481a0a10f794deae8a
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-11-25 07:09:52 +0800

    feat: Make current score default to 0 if currentScore is null

--- src/Scoreboard.jsx
+++ src/Scoreboard.jsx
@@ -1,7 +1,7 @@
 export default function Scoreboard({ currentScore, highScore }) {
     return (
         <div className="scoreboard">
-            <div className="current-score">Current Score: {currentScore}</div>
+            <div className="current-score">Current Score: {currentScore ?? 0}</div>
             <div className="high-score">High Score: {highScore}</div>
         </div>
     );
