0df47d1a30b42c8ce720083606bd102660c29036
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-11-25 08:29:28 +0800

    feat: Add button to hide or show instruction

--- src/Instruction.jsx
+++ src/Instruction.jsx
@@ -1,16 +1,36 @@
+import { useState } from 'react';
+
 export default function Instruction() {
+    const [isInstructionVisible, setIsInstructionVisible] = useState(false);
+
+    function handleClick() {
+        if (isInstructionVisible) {
+            setIsInstructionVisible(false);
+        } else {
+            setIsInstructionVisible(true);
+        }
+    }
+
     return (
-        <div className="instruction">
-            <div className="description">
-                <p>Welcome to Rickstatic Recall</p>
-                <p>
-                    Flip one card at a time, and try not to click the same card
-                    twice.
-                </p>
-                <p>
-                    Keep track of your score and challenge yourself to improve!
-                </p>
-            </div>
-        </div>
+        <>
+            <button className="instruction-button" onClick={handleClick}>
+                {isInstructionVisible ? 'Hide Instruction' : 'Show Instruction'}
+            </button>
+            {isInstructionVisible && (
+                <div className="instruction">
+                    <div className="description">
+                        <p>Welcome to Rickstatic Recall</p>
+                        <p>
+                            Flip one card at a time, and try not to click the
+                            same card twice.
+                        </p>
+                        <p>
+                            Keep track of your score and challenge yourself to
+                            improve!
+                        </p>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
