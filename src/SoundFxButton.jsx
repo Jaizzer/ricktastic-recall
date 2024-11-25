ee7e827f1fec9f8d0188e4bc5799c3f70f7a821b
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-11-25 08:02:59 +0800

    feat: Add button to Play or Pause background music

--- src/SoundFxButton.jsx
+++ src/SoundFxButton.jsx
@@ -1,12 +1,35 @@
+import { useRef, useState } from 'react';
 import backgroundMusic from './assets/background-music.mp3';
 
 export default function SoundFXButton() {
+    const [isPlaying, setIsPlaying] = useState(true);
+    const backgroundMusicRef = useRef(null);
+
+    function handleClick() {
+        if (backgroundMusicRef.current.paused) {
+            backgroundMusicRef.current.play();
+            setIsPlaying(true);
+        } else {
+            backgroundMusicRef.current.pause();
+            setIsPlaying(false);
+        }
+    }
+
     return (
-        <audio
-            src={backgroundMusic}
-            loop="true"
-            preload="auto"
-            autoPlay="true"
-        ></audio>
+        <>
+            <button
+                className="bg-music-switch"
+                onClick={handleClick}
+            >
+                {isPlaying ? 'Pause' : 'Play'}
+            </button>
+            <audio
+                ref={backgroundMusicRef}
+                src={backgroundMusic}
+                loop={true}
+                preload="auto"
+                autoPlay={true}
+            ></audio>
+        </>
     );
 }
