b57d657d251d291efa45607fa33bad816205c372
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-11-25 08:20:47 +0800

    feat: Create instruction component to help user learn the game

---
+++ src/Instruction.jsx
@@ -0,0 +1,16 @@
+export default function Instruction() {
+    return (
+        <div className="instruction">
+            <div className="description">
+                <p>Welcome to Rickstatic Recall</p>
+                <p>
+                    Flip one card at a time, and try not to click the same card
+                    twice.
+                </p>
+                <p>
+                    Keep track of your score and challenge yourself to improve!
+                </p>
+            </div>
+        </div>
+    );
+}
