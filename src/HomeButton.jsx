e2203fba173494c45130fd958350e1bce7f702bd
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-11-28 19:01:44 +0800

    feat: Add button click sound to home

--- src/HomeButton.jsx
+++ src/HomeButton.jsx
@@ -1,6 +1,19 @@
+import { useRef } from 'react';
+import buttonClickSfx from './assets/button-click.wav';
+
 export default function HomeButton({ goToHome, areSfxEnabled }) {
+    const buttonClickSfxRef = useRef(new Audio(buttonClickSfx));
     return (
-        <div className="home-button" onClick={goToHome}>
+        <div
+            className="home-button"
+            onClick={() => {
+                
+                if (areSfxEnabled) {
+                    buttonClickSfxRef.current.play();
+                }
+                goToHome();
+            }}
+        >
             <img src="" alt="Home" className="home-button-icon"></img>
             <div className="home-button-text">Memory Quest</div>
         </div>
