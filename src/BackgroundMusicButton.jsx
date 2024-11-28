0b8d8e41c91bed04623f9f8d4a32b6f3b24aca50
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-11-28 19:10:20 +0800

    feat: Add click button sound to background music button

--- src/BackgroundMusicButton.jsx
+++ src/BackgroundMusicButton.jsx
@@ -1,11 +1,19 @@
 import { useRef, useState } from 'react';
 import backgroundMusic from './assets/background-music.mp3';
+import buttonClickSfx from './assets/button-click.wav';
 
-export default function BackgroundMusicButton() {
+export default function BackgroundMusicButton({ areSfxEnabled }) {
     const [isPlaying, setIsPlaying] = useState(false);
     const backgroundMusicRef = useRef(new Audio(backgroundMusic));
 
+    const buttonClickSfxRef = useRef(new Audio(buttonClickSfx));
+
     function handleClick() {
+        // Play button sound fx if sfx are enabled
+        if (areSfxEnabled) {
+            buttonClickSfxRef.current.play();
+        }
+
         if (backgroundMusicRef.current.paused) {
             backgroundMusicRef.current.play();
             setIsPlaying(true);
