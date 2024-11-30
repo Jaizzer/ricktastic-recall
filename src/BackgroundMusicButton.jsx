60d76b5a5cd1929b746e527e9816237fcda76cf8
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-11-30 10:02:06 +0800

    feat: Disable music on first render for user experience

--- src/BackgroundMusicButton.jsx
+++ src/BackgroundMusicButton.jsx
@@ -3,15 +3,10 @@ import backgroundMusic from './assets/background-music.mp3';
 import buttonClickSfx from './assets/button-click.wav';
 
 export default function BackgroundMusicButton({ areSfxEnabled }) {
-    const [isPlaying, setIsPlaying] = useState(true);
+    const [isPlaying, setIsPlaying] = useState(false);
     const backgroundMusicRef = useRef(new Audio(backgroundMusic));
 
     const buttonClickSfxRef = useRef(new Audio(buttonClickSfx));
-
-    // Play background music on first re-render
-    useEffect(() => {
-        backgroundMusicRef.current.play();
-    }, [])
 
     function handleClick() {
         // Play button sound fx if sfx are enabled
