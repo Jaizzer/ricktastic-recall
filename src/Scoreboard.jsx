78c56f9e81efe65db5576e48fe731d571b7eb858
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-11-30 09:23:55 +0800

    feat: Update scoreboard appearance

--- src/Scoreboard.jsx
+++ src/Scoreboard.jsx
@@ -1,8 +1,14 @@
 export default function Scoreboard({ currentScore, highScore }) {
     return (
         <div className="scoreboard">
-            <div className="current-score">Current Score: {currentScore ?? 0}</div>
-            <div className="high-score">High Score: {highScore}</div>
+            <div className="score-slot">
+                <div className="title">High Score</div>
+                <div className="value">{highScore ?? 0}</div>
+            </div>
+            <div className="score-slot">
+                <div className="title">Current Score</div>
+                <div className="value">{currentScore ?? 0}</div>
+            </div>
         </div>
     );
 }
