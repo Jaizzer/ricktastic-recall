14b4e23faccbeed5ca1500358c960fe2f2261026
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-12-04 06:57:12 +0800

    feat: Add rick and morty image on instruction

--- src/Instruction.jsx
+++ src/Instruction.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from 'react';
 import buttonClickSfx from './assets/button-click.wav';
+import rickAndMorty from './assets/rick-and-morty.png';
 
 export default function Instruction({ areSfxEnabled }) {
     const [isInstructionVisible, setIsInstructionVisible] = useState(false);
@@ -25,21 +26,30 @@ export default function Instruction({ areSfxEnabled }) {
                 {isInstructionVisible ? 'Hide Instruction' : 'Show Instruction'}
             </button>
             {isInstructionVisible && (
-                <div className="instruction">
-                    <button
-                        className="close-instruction-button"
-                        onClick={handleClick}
-                    >x</button>
-                    <div className="description">
-                        <p>
-                            Wubba Lubba Dub-Dub! Flip cards, but don’t click the
-                            same one twice!
-                        </p>
-                        <p>
-                            Improve your score or quit—just kidding! Or am I?
-                            Good luck!
-                        </p>
+                <div className="instruction-container">
+                    <div className="instruction">
+                        <button
+                            className="close-instruction-button"
+                            onClick={handleClick}
+                        >
+                            x
+                        </button>
+                        <div className="description">
+                            <p>
+                                Wubba Lubba Dub-Dub! Flip cards, but don’t click
+                                the same one twice!
+                            </p>
+                            <p>
+                                Improve your score or quit—just kidding! Or am
+                                I? Good luck!
+                            </p>
+                        </div>
                     </div>
+                    <img
+                        src={rickAndMorty}
+                        onClick={handleClick}
+                        className="rick-and-morty"
+                    />
                 </div>
             )}
         </>
