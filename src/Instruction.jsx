51ea57287664454dee78ad68d97a71354494b8e0
Jaizzer <florenzjaizzer.calderon@gmail.com>
2024-12-04 10:08:43 +0800

    ref: Clean code and add comments

--- src/Instruction.jsx
+++ src/Instruction.jsx
@@ -5,28 +5,30 @@ import instructionIcon from './assets/show-instruction.png';
 import closeIcon from './assets/close.png';
 
 export default function Instruction({ areSfxEnabled }) {
+    // State to control visibility of the instruction popup
     const [isInstructionVisible, setIsInstructionVisible] = useState(false);
 
+    // Ref to manage the button click sound effect
     const buttonClickSfxRef = useRef(new Audio(buttonClickSfx));
 
+    // Function to handle the button click (toggle instructions visibility and play sound)
     function handleClick() {
-        // Play button sound fx if sfx are enabled
+        // Play button sound effect if sound effects are enabled
         if (areSfxEnabled) {
             buttonClickSfxRef.current.play();
         }
 
-        if (isInstructionVisible) {
-            setIsInstructionVisible(false);
-        } else {
-            setIsInstructionVisible(true);
-        }
+        // Toggle instruction visibility
+        setIsInstructionVisible((prevState) => !prevState);
     }
 
     return (
         <>
+            {/* Button to toggle instruction visibility */}
             <button className="instruction-button" onClick={handleClick}>
                 <img
                     src={isInstructionVisible ? closeIcon : instructionIcon}
+                    alt="Toggle Instructions"
                     className={
                         isInstructionVisible
                             ? 'close-instruction-button-icon'
@@ -34,18 +36,24 @@ export default function Instruction({ areSfxEnabled }) {
                     }
                 />
             </button>
+
+            {/* Instruction popup, displayed when visible */}
             {isInstructionVisible && (
                 <div className="instruction-container">
                     <div className="instruction">
+                        {/* Close button inside the popup */}
                         <button
                             className="close-instruction-button"
                             onClick={handleClick}
                         >
                             <img
                                 src={closeIcon}
+                                alt="Close Instructions"
                                 className="close-instruction-button-icon"
                             />
                         </button>
+
+                        {/* Instruction text description */}
                         <div className="description">
                             <p>
                                 Wubba Lubba Dub-Dub! Flip cards, but don’t click
@@ -57,8 +65,11 @@ export default function Instruction({ areSfxEnabled }) {
                             </p>
                         </div>
                     </div>
+
+                    {/* Rick and Morty image, also closes the popup on click */}
                     <img
                         src={rickAndMorty}
+                        alt="Rick and Morty Illustration"
                         onClick={handleClick}
                         className="rick-and-morty"
                     />
